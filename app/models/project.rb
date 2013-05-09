8c4fbd91d9bb2e43fbe19ad524c8ca8885e2c915
Michael Cartee <cartee07@gmail.com>
2013-05-08 22:00:36 -0600

    Added form validation for Projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,12 @@
 class Project < ActiveRecord::Base
   attr_accessible :description, :duration, :name, :status
+  	validates :name, :presence => true,
+  				     :uniqueness => true
+
+  	validates :description, :length => {
+  		:minimum => 5,
+  		:maximum => 250}
+
+  	validates :duration, :presence => true
+  	validates :status, :presence => true	
 end
